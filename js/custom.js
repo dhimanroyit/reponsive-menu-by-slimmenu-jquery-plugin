eee306572a09347845b3473bc39baa5be938f9ff
dhimanroyit <dhimanroyit@gmail.com>
2018-07-24 04:38:42 +0600

    responsive menu by slimmenu jquery plugin

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('.nav__ul').slimmenu({
+    resizeWidth: '900',
+    collapserTitle: '',
+    animSpeed: 'medium',
+    easingEffect: null,
+    indentChildren: true,
+    childrenIndenter: '&nbsp;'
+  });
+
+})
